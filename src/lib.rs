fbd21c40cec5bc7ae84cb6a20fb180b3c41b5c98
snghojeong <snghojeong@gmail.com>
2020-08-19 19:34:33 +0900

    Modify the name of module to geolo_view

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn string_sum(py: Python, m: &PyModule) -> PyResult<()> {
+fn geolo_view(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
 
     Ok(())

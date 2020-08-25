45d02d08e0759e8687e675a40353d8b06c6d6a64
snghojeong <snghojeong@gmail.com>
2020-08-25 21:40:20 +0900

    Modify PYO3 interface to get filename

--- geolo_viewer.py
+++ geolo_viewer.py
@@ -1,4 +1,4 @@
 import geolo_view
 
-ret = geolo_view.sum_as_string(10, 10)
+ret = geolo_view.read_log("jup_log")
 print(ret)
--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,14 @@ use pyo3::wrap_pyfunction;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn read_log(filename: String) -> PyResult<String> {
+    Ok(filename)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn geolo_view(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
+    m.add_wrapped(wrap_pyfunction!(read_log))?;
 
     Ok(())
 }

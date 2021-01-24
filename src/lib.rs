2d938a0768faac42016d3dbb8a2412609a0a97e6
snghojeong <snghojeong@gmail.com>
2021-01-24 21:13:35 +0900

    Implement read_log_line method of LogReader

--- src/lib.rs
+++ src/lib.rs
@@ -6,35 +6,12 @@ use std::io::SeekFrom;
 use std::io::BufReader;
 use std::io::Result;
 
-struct LogReader {
-    reader: BufReader<File>,
-    line_buf: String,
-}
-
-impl LogReader {
-    fn open(path: &str) -> Result<Self> {
-        let mut file = File::open(path)?;
-        let inst = LogReader {
-            reader: BufReader::new(file),
-            line_buf: String::new(),
-        };
-        Ok(inst)
-    }
-
-    fn read_log_line(&mut self, reader: &mut BufReader<File>) -> Result<String> {
-        let mut log_buf = String::new();
-        reader.read_line(&mut self.line_buf)?;
-        log_buf.push_str(self.line_buf.as_str());
-        Ok(log_buf)
-    }
-}
-
 fn idx(log_line: &str) -> &str {
     &log_line[0..3]
 }
 
 fn date(log_line: &str) -> &str {
-    &log_line[5..10]
+    &log_line[5..11]
 }
 
 fn level(log_line: &str) -> &str {
@@ -70,11 +47,53 @@ fn is_log_line(log_line: &str) -> bool {
 }
 
 fn filter_log<'a>(log_line: &'a String, lv: &'a String, md: &'a String) -> Option<&'a String> {
-    if is_log_line(log_line.as_str()) && (level(log_line.as_str()).trim() == lv.as_str()) {
+    if level(log_line.as_str()).trim() == lv.as_str() {
         Some(log_line)
     }
     else {
         None
+    }
+}
+
+struct LogReader {
+    reader: BufReader<File>,
+    line_buf: String,
+}
+
+impl LogReader {
+    fn open(path: &str) -> Result<Self> {
+        let mut file = File::open(path)?;
+        let mut reader = BufReader::new(file);
+        let mut line_buf = String::new();
+
+        loop {
+            reader.read_line(&mut line_buf)?;
+            if is_log_line(line_buf.as_str()) {
+                break;
+            }
+        }
+
+        let inst = LogReader {
+            reader: reader,
+            line_buf: line_buf,
+        };
+
+        Ok(inst)
+    }
+
+    fn read_log_line(&mut self) -> Result<String> {
+        let mut log_line = String::new();
+
+        loop {
+            log_line.push_str(self.line_buf.as_str());
+            self.line_buf.clear();
+            self.reader.read_line(&mut self.line_buf)?;
+            if is_log_line(self.line_buf.as_str()) {
+                break;
+            }
+        }
+
+        Ok(log_line)
     }
 }
 

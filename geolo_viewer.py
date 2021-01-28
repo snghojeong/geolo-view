90c0c6de5fdad7d5d7553225c14b65e18b6fe7db
snghojeong <snghojeong@gmail.com>
2021-01-28 10:48:41 +0900

    Modify py-script to support option param

--- geolo_viewer.py
+++ geolo_viewer.py
@@ -1,5 +1,22 @@
 import geolo_view
 import sys
+import argparse
 
-ret = geolo_view.read_log(sys.argv[1], 0, int(sys.argv[2]), sys.argv[3], sys.argv[4], False)
+parser = argparse.ArgumentParser(description='Log reader')
+parser.add_argument('--file', required=True, help='log file')
+parser.add_argument('--cnt', required=True, help='the number of log line')
+parser.add_argument('--lv', required=False, help='log level to filter')
+parser.add_argument('--md', required=False, help='module name to filter')
+
+args = parser.parse_args()
+
+lv_str = ""
+if args.lv:
+    lv_str = args.lv
+
+md_str = ""
+if args.md:
+    md_str = args.md
+
+ret = geolo_view.read_log(args.file, 0, int(args.cnt), lv_str, md_str, False)
 print(ret)

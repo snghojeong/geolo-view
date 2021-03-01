1163de95fd78dd602901c5315aa87affbd4eca12
snghojeong <snghojeong@gmail.com>
2021-03-01 21:52:45 +0900

    Modify viewer.py to print more logs

--- geolo_viewer.py
+++ geolo_viewer.py
@@ -10,14 +10,21 @@ parser.add_argument('--md', required=False, help='module name to filter')
 
 args = parser.parse_args()
 
-if args.md and args.lv:
-    ret = geolo_view.read_log(args.file, 0, int(args.cnt), False, lv=args.lv, md=args.md)
-elif args.md:
-    ret = geolo_view.read_log(args.file, 0, int(args.cnt), False, md=args.md)
-elif args.lv:
-    ret = geolo_view.read_log(args.file, 0, int(args.cnt), False, lv=args.lv)
-else:
-    ret = geolo_view.read_log(args.file, 0, int(args.cnt), False)
-
-print(ret["log"])
-print(ret["pos"])
+prog_exit = 0
+pos = 0
+while prog_exit == 0:
+    if args.md and args.lv:
+        ret = geolo_view.read_log(args.file, pos, int(args.cnt), False, lv=args.lv, md=args.md)
+    elif args.md:
+        ret = geolo_view.read_log(args.file, pos, int(args.cnt), False, md=args.md)
+    elif args.lv:
+        ret = geolo_view.read_log(args.file, pos, int(args.cnt), False, lv=args.lv)
+    else:
+        ret = geolo_view.read_log(args.file, pos, int(args.cnt), False)
+    print(ret["log"])
+    keyin = input('Exit(q):')
+    if keyin == 'q':
+        prog_exit = 1
+    elif ret["log"] == "":
+        prog_exit = 1
+    pos = ret["pos"]

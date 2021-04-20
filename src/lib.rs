8cfb70c408049b00064dd9b5ae3e4d5274b3b928
snghojeong <snghojeong@gmail.com>
2021-04-20 22:03:39 +0900

    Modify filter_log to use is_matched function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 use pyo3::types::PyDict;
-use std::str::Split;
 
 mod log_reader;
 
@@ -23,55 +22,19 @@ fn is_matched(kwds: &Option<Vec<String>>, log_field: &str) -> bool {
 
 fn filter_log<'a>(log_line: &'a String, 
                   seq: &'a Option<Vec<String>>, 
+                  date: &'a Option<Vec<String>>, 
                   lv: &'a Option<Vec<String>>, 
                   qlabel: &'a Option<Vec<String>>, 
                   md: &'a Option<Vec<String>>, 
                   msg: &'a Option<Vec<String>>) -> Option<&'a String> {
-    let mut is_match_seq = is_matched(seq, log_reader::seq(log_line.as_str()));
+    let is_match_seq = is_matched(seq, log_reader::seq(log_line.as_str()));
+    let is_match_date = is_matched(date, log_reader::date(log_line.as_str()));
+    let is_match_lv = is_matched(lv, log_reader::level(log_line.as_str()));
+    let is_match_qlabel = is_matched(qlabel, log_reader::qlabel(log_line.as_str()));
+    let is_match_md = is_matched(md, log_reader::mod_name(log_line.as_str()));
+    let is_match_msg = is_matched(msg, log_reader::msg(log_line.as_str()));
 
-    let mut is_match_md = false;
-    match (md) {
-        None => { 
-            is_match_md = true;
-        },
-        Some(md_str) => {
-            for s in md_str {
-                if log_reader::mod_name(log_line.as_str()).contains(s) {
-                    is_match_md = true;
-                }
-            }
-        }
-    }
-
-    let mut is_match_lv = false;
-    match (lv) {
-        None => { 
-            is_match_lv = true;
-        },
-        Some(lv_str) => {
-            for s in lv_str {
-                if log_reader::level(log_line.as_str()).contains(s) {
-                    is_match_lv = true;
-                }
-            }
-        }
-    }
-
-    let mut is_match_msg = false;
-    match (msg) {
-        None => { 
-            is_match_msg = true;
-        },
-        Some(msg_str) => {
-            for s in msg_str {
-                if log_reader::msg(log_line.as_str()).contains(s) {
-                    is_match_msg = true;
-                }
-            }
-        }
-    }
-
-    if is_match_seq && is_match_lv && is_match_md && is_match_msg {
+    if is_match_seq && is_match_date && is_match_lv && is_match_qlabel && is_match_md && is_match_msg {
         Some(log_line)
     }
     else {
@@ -94,6 +57,7 @@ fn read_log(py: Python, path: String, pos: u64, line_cnt: i32, kwds: Option<&PyD
     let mut log_buf = String::new();
 
     let seq = split_filter_keywords(kwds, "seq");
+    let date = split_filter_keywords(kwds, "date");
     let lv = split_filter_keywords(kwds, "lv");
     let qlabel = split_filter_keywords(kwds, "qlabel");
     let tid = split_filter_keywords(kwds, "tid");
@@ -106,7 +70,7 @@ fn read_log(py: Python, path: String, pos: u64, line_cnt: i32, kwds: Option<&PyD
         let log_line = reader.read_log_line();
         match (log_line) {
             Ok(unwrap_log_ln) => {
-                match (filter_log(&unwrap_log_ln, &seq, &lv, &qlabel, &md, &msg)) {
+                match (filter_log(&unwrap_log_ln, &seq, &date, &lv, &qlabel, &md, &msg)) {
                     Some(filtered_log) => {
                         log_buf.push_str(filtered_log.as_str());
                         pushed_cnt += 1;

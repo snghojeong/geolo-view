d817a8dafebd408445dc579b1ea471dabedc86ff
snghojeong <snghojeong@gmail.com>
2021-04-28 22:44:07 +0900

    Modify viewer to set seq and date

--- geolo_viewer.py
+++ geolo_viewer.py
@@ -5,6 +5,8 @@ import argparse
 parser = argparse.ArgumentParser(description='Log reader')
 parser.add_argument('--file', required=True, help='log file')
 parser.add_argument('--cnt', required=True, help='the number of log line')
+parser.add_argument('--seq', required=False, help='seq number to filter')
+parser.add_argument('--date', required=False, help='date to filter')
 parser.add_argument('--lv', required=False, help='log level to filter')
 parser.add_argument('--md', required=False, help='module name to filter')
 parser.add_argument('--msg', required=False, help='message string to filter')

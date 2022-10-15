66c5f1ca2440f5b08283cc4efe92d58c60998173
snghojeong <snghojeong@gmail.com>
2022-10-15 22:52:23 +0900

    Delete sequence

--- src/lib.rs
+++ src/lib.rs
@@ -68,14 +68,12 @@ fn is_in_time(kwds: &Option<Vec<String>>, log_time: ParseResult<NaiveTime>) -> P
 }
 
 fn filter_log<'a>(log_line: &'a String, 
-                  seq: &'a Option<Vec<String>>, 
                   date: &'a Option<Vec<String>>, 
                   time: &'a Option<Vec<String>>, 
                   lv: &'a Option<Vec<String>>, 
                   qlabel: &'a Option<Vec<String>>, 
                   md: &'a Option<Vec<String>>, 
                   msg: &'a Option<Vec<String>>) -> Option<&'a String> {
-    let is_match_seq = is_matched(seq, log_reader::seq(log_line.as_str()));
     let is_match_date = is_matched(date, log_reader::date(log_line.as_str()));
     let is_match_time = is_in_time(time, log_reader::time(log_line.as_str())).unwrap_or(true);
     let is_match_lv = is_matched(lv, log_reader::level(log_line.as_str()));
@@ -83,7 +81,7 @@ fn filter_log<'a>(log_line: &'a String,
     let is_match_md = is_matched(md, log_reader::mod_name(log_line.as_str()));
     let is_match_msg = is_matched(msg, log_reader::msg(log_line.as_str()));
 
-    if is_match_seq && is_match_date && is_match_time && is_match_lv && is_match_qlabel && is_match_md && is_match_msg {
+    if is_match_date && is_match_time && is_match_lv && is_match_qlabel && is_match_md && is_match_msg {
         Some(log_line)
     }
     else {
@@ -105,7 +103,6 @@ fn read_log(py: Python, path: String, pos: u64, line_cnt: i32, kwds: Option<&PyD
     let mut reader = log_reader::LogReader::open(path.as_str(), pos)?;
     let mut log_buf = String::new();
 
-    let seq = split_filter_keywords(kwds, "seq");
     let date = split_filter_keywords(kwds, "date");
     let time = split_filter_keywords(kwds, "time");
     let lv = split_filter_keywords(kwds, "lv");
@@ -118,7 +115,7 @@ fn read_log(py: Python, path: String, pos: u64, line_cnt: i32, kwds: Option<&PyD
         let log_line = reader.read_log_line();
         match log_line {
             Ok(unwrap_log_ln) => {
-                match filter_log(&unwrap_log_ln, &seq, &date, &time, &lv, &qlabel, &md, &msg) {
+                match filter_log(&unwrap_log_ln, &date, &time, &lv, &qlabel, &md, &msg) {
                     Some(filtered_log) => {
                         log_buf.push_str(filtered_log.as_str());
                         pushed_cnt += 1;
